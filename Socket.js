836abe9265220268898b247559c5569cdecb6f00
Bruno Sanches <brunopsanches@gmail.com>
2014-03-25 23:39:08 -0700

    Define Socket, an extremely simple event emmiter to fake a socket stream

---
+++ Socket.js
@@ -0,0 +1,25 @@
+/* global module */
+module.exports = (function () {
+  'use strict';
+
+  function Socket() {
+    this.init();
+  }
+
+  Socket.prototype.init = function () {
+    this.events = {};
+  };
+
+  Socket.prototype.on = function (event, cb) {
+    this.events[event] = this.events[event] || [];
+    this.events[event].push(cb);
+  };
+
+  Socket.prototype.emit = function (event, data) {
+    for (var i = 0; i < this.events[event].length; ++i) {
+      this.events[event][i](data);
+    }
+  };
+
+  return Socket;
+}());
