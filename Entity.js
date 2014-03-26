fdf564d9a0f7015a6320af7bb8a4400096213c84
Bruno Sanches <brunopsanches@gmail.com>
2014-03-26 00:34:00 -0700

    Entity state can be updated with an input on new #applyInput method
    * Define entity speed in pixels per second
    * input.deltaModifier is a time difference value since last input to
    make sure entity moves accondingly, regardless of client frame rate

--- Entity.js
+++ Entity.js
@@ -10,10 +10,31 @@ module.exports = (function () {
     this.x = opts.x || 0;
     this.y = opts.y || 0;
 
+    // speed in pixels per second
+    this.speed = opts.speed || 150;
+
     this.color = opts.color || '#00f';
     this.borderSize = opts.borderSize || 3;
     this.borderColor = opts.borderColor || '#000';
     this.radius = opts.radius || 10;
+  };
+
+  Entity.prototype.applyInput = function (input) {
+    if (input.keys.LEFT) {
+      this.x -= this.speed * input.deltaModifier;
+    }
+
+    if (input.keys.RIGHT) {
+      this.x += this.speed * input.deltaModifier;
+    }
+
+    if (input.keys.DOWN) {
+      this.y -= this.speed * input.deltaModifier;
+    }
+
+    if (input.keys.UP) {
+      this.y += this.speed * input.deltaModifier;
+    }
   };
 
   Entity.prototype.render = function (ctx) {
