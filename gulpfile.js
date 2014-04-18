77d66029b8ec28ecde15a52fa886fecc9e6bb50e
Bruno Sanches <brunopsanches@gmail.com>
2014-04-17 18:27:07 -0700

    Automatically open application as part o build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var open = require('gulp-open');
 
 gulp.task('scripts', function () {
   return browserify('./js/world.js')
@@ -17,6 +18,18 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
   gulp.watch('./js', ['scripts']);
   gulp.watch('./*.html', ['html']);
-})
+});
 
-gulp.task('default', ['scripts', 'html', 'watch']);
+gulp.task('open', function () {
+  var options = {
+    app: 'google chrome'
+  };
+
+  return gulp.src('./build/index.html')
+    .pipe(open('./build/index.html', options));
+});
+
+gulp.task('default', ['scripts', 'html', 'watch', 'open']);
+
+
+
